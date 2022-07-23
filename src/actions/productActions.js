c6049dd423ca1666aac001287f5cf09f959d9b0d
arijitdas13105 <arijitdas13105@gmail.com>
2022-07-24 01:20:13 +0530

    changed

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -12,7 +12,8 @@ export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
     // const { data } = await axios.get("api/products");
-    const { data } = await axios.get("https://fakestoreapi.com/products");
+    // const { data } = await axios.get("https://fakestoreapi.com/products");
+    const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
   //  const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
     // console.log('api/products', data);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
