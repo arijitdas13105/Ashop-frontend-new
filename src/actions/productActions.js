14125311771abb95bb0d6144e84a2546ea889d63
arijitdas13105 <arijitdas13105@gmail.com>
2022-07-23 15:24:09 +0530

    new change

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -3,7 +3,7 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAILS,
-  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_REQUEST,   
   PRODUCT_DETAILS_SUCCESS,
 } from "../constants/productConstant";
 
@@ -11,8 +11,8 @@ import {
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
-    // const { data } = await axios.get("api/products");
-    const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
+    const { data } = await axios.get("api/products");
+  //  const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
     console.log('api/products', data);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
   } catch (error) {
