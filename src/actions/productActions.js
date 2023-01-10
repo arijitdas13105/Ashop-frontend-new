5390c3121fe0449ef449fc5d8199f37ba8144bb8
Arijit Das <62784803+arijitdas13105@users.noreply.github.com>
2023-01-10 21:54:22 +0530

    changing the server to render

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -13,7 +13,7 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_REQUEST });
     // const { data } = await axios.get("api/products");
     // const { data } = await axios.get("https://fakestoreapi.com/products");
-    const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
+    const { data } = await axios.get("https://arijitdas13105-ashop-backend.onrender.com//api/products");
   //  const { data } = await axios.get("https://ashop-server.herokuapp.com/api/products");
     // console.log('api/products', data);
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
@@ -25,7 +25,7 @@ export const listProducts = () => async (dispatch) => {
 export const listProductDetails=(id)=>async(dispatch)=> {
   try {
     dispatch({type:PRODUCT_DETAILS_REQUEST})
-    const {data }= await axios.get(`https://ashop-server.herokuapp.com/api/product/${id}`)
+    const {data }= await axios.get(`https://arijitdas13105-ashop-backend.onrender.com//api/product/${id}`)
     dispatch({
         type:PRODUCT_DETAILS_SUCCESS,
         payload :data
